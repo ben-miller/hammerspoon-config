d8d76db43f21a845f8014c39cff25fdb656d6b7f
Benjamin Miller <benmiller179@gmail.com>
2022-02-03 09:34:19 +0900

    Update

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -201,5 +201,22 @@ hs.hotkey.bind(hypo, "T", function()
   hs.urlevent.openURL(url)
 end)
 
+-- Kill Apple Music as soon as it opens
+function applicationMusicWatcher(appName, eventType, appObject)
+    if (eventType == hs.application.watcher.launching) then
+        if (appName == "Music") then
+            local op, stat, typ, ec = hs.execute([["/usr/bin/killall" "Music"]])
+            hs.alert.show("Killed Apple Music")
+            if not (ec == 0) then
+                msg = "An error occurred terminating Music."
+                hs.notify.new({title="Hammerspoon", informativeText=msg}):send()
+            end                     
+        end
+    end
+end
+appMusicWatcher = hs.application.watcher.new(applicationMusicWatcher)
+appMusicWatcher:start()
+
 -- Announce that config has changed
 hs.alert.show("Config loaded")
+
