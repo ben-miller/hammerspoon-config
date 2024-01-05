74537b655d3d6408fa0c4c455c666d7242dde13f
Benjamin Miller <benmiller179@gmail.com>
2024-01-05 13:37:52 -0500

    feat: Add function to hide all windows

--- .hammerspoon/contexts.lua
+++ .hammerspoon/contexts.lua
@@ -1,10 +1,26 @@
+local log = hs.logger.new("main", "verbose") -- Use 'log.e(xxx)'
+
 local contexts = {}
 
+function hideAllWindows()
+    local allWindows = hs.window.allWindows()
+    for _, window in ipairs(allWindows) do
+        local app = window:application()
+        if app then app:hide() end
+    end
+end
+
 function contexts.loadWritingContext()
+
+    -- Hide all apps
+    hideAllWindows()
 
     -- Open writing context in Obsidian
     local writingContextUrl = "obsidian://open?vault=life&file=Areas%2FLeisure%2FWriting%2FWriting%20Context"
     hs.urlevent.openURL(writingContextUrl)
+
+    -- Open Timer app
+    hs.application.launchOrFocus("Timer")
 
     -- Show alert
     hs.alert.show("Writing context loaded")
