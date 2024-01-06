5b5b3a022b824057a55cce21ee4e1ebcf643d91b
Benjamin Miller <benmiller179@gmail.com>
2024-01-05 23:44:55 -0500

    refactor: simplify code for generating HTML files

--- .hammerspoon/shortcuts.lua
+++ .hammerspoon/shortcuts.lua
@@ -2,6 +2,47 @@ local shortcuts = {}
 
 local applications = require("./applications")
 local home = os.getenv("HOME")
+local htmlTemplateFile = home .. "/src/infra/config/hammerspoon/.hammerspoon/template.html"
+
+-- Search and replace patterns in html template string
+function simpleTemplate(str, data)
+    return (str:gsub('($%b{})', function(w)
+        return data[w:sub(3, -2)] or w
+    end))
+end
+
+function readHtmlFile(filePath)
+    local file = io.open(filePath, "r")  -- Open the file for reading
+    if not file then return nil, "Could not open file: " .. filePath end
+    local content = file:read("*a")  -- Read the entire content
+    file:close()
+    return content
+end
+
+function genHtmlForApp(app)
+
+    -- Open html file for app
+    local template = readHtmlFile(htmlTemplateFile)
+
+    -- Values to replace in template
+    local data = {
+        ["shortName"] = app.shortName,
+        ["appName"] = app.appName
+    }
+
+    -- Replace values
+    local html = simpleTemplate(template, data)
+
+    -- Write to file
+    local filename = shortcuts.htmlFileForApp(app.shortName)
+    local file = io.open(filename, "w")
+    if not file then
+        hs.alert.show("Could not open file " .. filename)
+        return
+    end
+    file:write(html)
+    file:close()
+end
 
 -- Take an application and output a dummy html file into ./shortcuts
 -- This file will be named based on the application name.
@@ -9,33 +50,7 @@ function shortcuts.generateHtml()
 
     -- iterate over all applications
     for _, app in pairs(applications.all) do
-        local name = app.shortName
-        local html = [[
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <meta charset="utf-8">
-              <title>]] .. name .. [[</title>
-              <link rel="stylesheet" href="style.css">
-            </head>
-            <body>
-              <div class="container">
-                <div class="icon">
-                </div>
-                <div class="name">]] .. name .. [[</div>
-              </div>
-            </body>
-          </html>
-        ]]
-    
-        local filename = home .. "/src/infra/config/hammerspoon/.hammerspoon/shortcuts/" .. name .. ".html"
-        local file = io.open(filename, "w")
-        if not file then
-            hs.alert.show("Cou/d not open file " .. filename)
-            return
-        end
-        file:write(html)
-        file:close()
+        genHtmlForApp(app)
     end
 end
 
